d5eb88713bdb6b3008743fddf67250f6242400e4
Fabio Palumbo <fabio.palumbo@electric.ai>
2020-08-07 11:37:26 -0300

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const NUMBER_OF = {
+  ARTICLES: 5,
+  PHOTOS: 4,
+  VIDEOS: 3
+}
+
+export const PLACEHOLDERS = {
+  LATEST_ARTICLES: "%{{latest_articles}}%",
+  LATEST_YOUTUBE: "%{{latest_youtube}}%",
+  LATEST_INSTAGRAM: "%{{latest_instagram}}%"
+  LATEST_LINKDN: "%{{latest_linkdn}}%"
+}
